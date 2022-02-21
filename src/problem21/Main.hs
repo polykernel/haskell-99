9b24697995a857353d6bf427fa65b7045e9b1f2a
polykernel <81340136+polykernel@users.noreply.github.com>
2022-02-21 00:08:09 -0500

    chore: revise solution
    - Correct pattern matching order for problem 21. The previous order does not
      allow insertion at the end of a list due to the first pattern being matched.

--- src/problem21/Main.hs
+++ src/problem21/Main.hs
@@ -1,10 +1,10 @@
 insertAt :: a -> [a] -> Int -> [a]
-insertAt _ [] _ = []
 insertAt z xs 1 = z : xs
+insertAt _ [] _ = []
 insertAt z (x : xs) n = x : (insertAt z xs (n - 1))
 
 main :: IO ()
 main =
   print (insertAt 'X' "abcd" 2) >>
   print (insertAt 0 ([] :: [Int]) 2) >>
-  print (insertAt "abc" ["abcd", "efgh"] 1)
+  print (insertAt "abc" ["abcd", "efgh"] 3)
